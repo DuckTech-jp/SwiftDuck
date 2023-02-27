4373137c324ec5e4eb6712fc89171abf3cf4b723
DuckTech <ducktech.jpn@gmail.com>
2023-02-27 20:26:49 +0900

    Initial Commit

---
+++ SwiftDuck/SwiftDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDuckApp.swift
+//  SwiftDuck
+//
+//  Created by DuckTech on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
